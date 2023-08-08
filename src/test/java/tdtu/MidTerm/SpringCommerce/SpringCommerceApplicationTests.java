29d6a49f6772b62b90937a4e9e930a76e1813f51
Nguyen_Hoang_Danh <52100010@student.tdtu.edu.vn>
2023-08-09 02:53:13 +0700

    init project

---
+++ src/test/java/tdtu/MidTerm/SpringCommerce/SpringCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package tdtu.MidTerm.SpringCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
